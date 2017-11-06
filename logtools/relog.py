c45a05f334333f156dc2ca417febc7ab2c93ea1f
Oskar Skog <oskar.skog.finland@gmail.com>
2017-11-06 18:04:12 +0000

    Worked on the logging tools

--- logtools/relog.py
+++ logtools/relog.py
@@ -2,7 +2,7 @@
 
 import math
 
-def temperature(f):
+def temperature(f, calibration):
     '''
     Returns temperature in degrees celcius.
     Input is the measured frequency from the 555.
@@ -13,12 +13,27 @@ def temperature(f):
     D = 1/(2*ln(2)*C1)  # C1 is the capacitor
     E = R2/2            # R2 is the other resistor (between discharge and VDD)
     '''
-    A = 6.997 *10**-4   # Beta to Steinhart-Hart
-    B = 2.882 *10**-4   # Beta to Steinhart-Hart
-    C = 0               # Beta to Steinhart-Hart
-    D = 1.5 *10**5      # 4.7 uF
-    E = 1.1 *10**3      # 2.2 kOhm
-    
+    A, B, C, D, E = [calibration[key] for key in 'ABCDE']
     R = D/f - E
     T = 1/(A + B*math.log(R) + C*math.log(R)**3)
     return T-273.15
+
+def windspeed(x, calibration):
+    return x*calibration['k']
+
+def winddirection(a, calibration):
+    return a+calibration['north']
+
+
+def parseline(line):
+    '''Transform the newly read line.'''
+    pass
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
